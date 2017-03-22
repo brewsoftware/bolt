2a30d2255b51af6bf5fb114b3249c54fb0bcad70
Jason Turner <jturner@liberty.com.au>
2017-03-22 14:55:50 +1100

    linting updates

--- src/async-parser.ts
+++ src/async-parser.ts
@@ -2,77 +2,63 @@ let through = require('through2');
 let readfile = require('fs-readfile-promise');
 let path = require('path');
 let bolt = require('./rules-parser');
-
+let fs = require('fs');
 /*
   Async file parser for split file systems
   Note: Using a modified bolt syntax to include imports
 */
 const PLUGIN_NAME = 'firebase-async-parser';
-var sym; // Global symbols variable
-
-export function parseAsync(filename) {
+var sym : any; // Global symbols variable
+var cwd :string = '';
+export function parseAsync(filename: string) {
     // creating a stream through which each file will pass
-      var cwd ='';
       fs.createReadStream(filename)
-        .pipe(through(function(file, enc, cb) {
-        var self = this;
+        .pipe(through(function(file : any, enc : any, cb : any) {
         var newfile = file.clone({contents: false});
-        var content ='';
+        var content = '';
         // base relative path
         var cwdPath = file.cwd.split(path.sep);
         var histPath = file.history[0].split(path.sep);
-        for(var i=0;i< cwdPath.length ;i++){
+        for (let i = 0; i < cwdPath.length; i++) {
           histPath.shift();
         }
         histPath.pop();
         cwd = './';
-        if(histPath.length >0){
+        if (histPath.length > 0) {
           cwd = cwd + histPath.join('/');
         }
 
         if (file.isBuffer()) {
-          content = file.contents.toString()
-          parserWrapper(content).then(function(symbols){
-            readSuccess(symbols, cb,newfile );
+          content = file.contents.toString();
+          parserWrapper(content).then(function(symbols : any) {
+            readSuccess(symbols, cb, newfile);
           }).catch(function(ex){
-             this.emit('error', new PluginError(PLUGIN_NAME, 'Error converting file'));
+             //this.emit('error', new PluginError(PLUGIN_NAME, 'Error converting file'));
              cb();
           });
         } else if (file.isStream()) {
-          file.contents.on('data', function(chunk) {
+          file.contents.on('data', function(chunk : any) {
             content = content + chunk;
           });
-          file.contents.on('end', function(){
+          file.contents.on('end', function() {
             // make sure the file goes through the next gulp plugin
-            parserWrapper(content ).then(function(symbols){
-                readSuccess(symbols, cb,newfile );
-            }).catch(function(ex){
-               this.emit('error', new PluginError(PLUGIN_NAME, 'Error converting file'));
+            parserWrapper(content ).then(function(symbols : any) {
+                readSuccess(symbols, cb, newfile);
+            }).catch(function(ex) {
+               //this.emit('error', new PluginError(PLUGIN_NAME, 'Error converting file'));
                cb();
             });
           });
         } else {
           cb();
         }
-      });
-
-      // returning the file stream
-    return stream;
+      }));
   };
     /*
       *************************** Function Section ****************************
     */
 
-    function parsePath(path) {
-       var extname = Path.extname(path);
-       return {
-         dirname: Path.dirname(path),
-         basename: Path.basename(path, extname),
-         extname: extname
-       };
-     }
-
-    function parserWrapper(data) {
+    function parserWrapper(data: string) {
       var promises = [];
       sym = bolt.parse(data);
       var currentDirectoryContext = cwd;
@@ -97,46 +83,45 @@ export function parseAsync(filename) {
       return retPromise;
     }; // end function
 
-    function getNextFilenameFromContextAndImport(current, nextImport){
+    function getNextFilenameFromContextAndImport(current : any, nextImport : any) {
       // Convert absolute filenames to relative
       // Convert relative filenames to include original path
 
       var currentFn = current.filename.split('/');
       var nextFn = nextImport.filename.split('/');
-      if(nextImport.scope){
+      if (nextImport.scope) {
         nextImport.filename = './node_modules/' + nextImport.filename + '/index';
-      } else{
+      } else {
         // import {./something} -> ['.','something'] -> ''
         // import {./something/anotherthing} -> ['.','something','anotherthing'] -> something
 
         currentFn.pop();
-        while(nextFn[0] === '..'){
+        while (nextFn[0] === '..') {
           currentFn.pop();
           nextFn.shift();
         }
-        if(nextFn[0] === '.'){
+        if (nextFn[0] === '.') {
           nextFn.shift();
         }
         nextImport.filename  = currentFn.concat(nextFn).join('/');
       }
-
       return nextImport;
     }
 
-    function processRecursive(next){
+    function processRecursive(next : any) {
 
       var promises = [];
       var p = new Promise(function(resolve, reject) {
 
         console.log("Reading: " + JSON.stringify(next));
         readfile(next.filename  + '.bolt').then(
-          function(subData) {
+          function(subData : any) {
             console.log("Success Reading: " + next.filename + '.bolt');
             var subPromises = [];
             var newRules = bolt.parse(subData.toString());
             console.log('*******' + next.filename);
             if (newRules) {
-                newRules.imports.map(function (obj) {
+                newRules.imports.map(function (obj : any) {
                     sym.imports.push(obj);
                     return obj;
                 });
@@ -163,7 +148,7 @@ export function parseAsync(filename) {
               scope: fale = local
             }
             */
-            for(var impKey in newRules.imports){
+            for (var impKey in newRules.imports) {
                 var imp = getNextFilenameFromContextAndImport(next, newRules.imports[impKey]);
 
                 // check for global modules
@@ -171,9 +156,9 @@ export function parseAsync(filename) {
                 subPromises.push(inner);
                 // push a new import path
             }
-            Promise.all(subPromises).then(function(){
+            Promise.all(subPromises).then(function() {
               resolve();
-            }).carch(function(ex){
+            }).catch(function(ex : any){
               this.emit('error in parsed files');
             });
 
@@ -181,10 +166,10 @@ export function parseAsync(filename) {
       }); // end promise
     promises.push(p);
 
-    var retPromise = new Promise(function(resolve, reject){
+    var retPromise = new Promise(function(resolve, reject) {
       Promise.all(promises).then(function(){
         resolve();
-      }).catch(function(ex){
+      }).catch(function(ex : any){
         console.log(ex);
       });
     });
@@ -194,11 +179,11 @@ export function parseAsync(filename) {
     * readSuccess -Final call with all the symboly loaded
     * calls the original parser function after the parser has succeeded.
     */
-    function readSuccess(symbols, cb, newfile){
+    function readSuccess(symbols : any, cb : any, newfile : any) {
       var gen = new bolt.Generator(symbols);
       var rules = gen.generateRules();
       var output =  JSON.stringify(rules, null, 2);
       newfile.contents = new Buffer(output);
-      newfile.path = newfile.path.replace('.bolt','.json');
-      cb(null, newfile);// finished
+      newfile.path = newfile.path.replace('.bolt', '.json');
+      cb(null, newfile); // finished
     };
